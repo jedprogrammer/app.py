85813801845d43d2089c7e6c34e8eeb94a18dc22
jedprogrammer <jedprogrammered@gmail.com>
2025-07-01 13:02:35 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+import math
+student_count = 1000
+group_name = "calss of 2025"
+
+print(len(group_name))
+print(group_name[3])
+print(group_name. upper())
+print(group_name.strip())
+print(group_name.replace("a", "l"))
+print(group_name.find("2025"))
+
+x = 10
+x += 3
+print(x)
+
+print(math.copysign(-2.27, 12.78))
+
+
+x = input("x: ")
+y = int(x)+1
+print(f" x: {x},  y: {y} ")
---
+++ condition.py
@@ -0,0 +1,15 @@
+# temperature = 14
+# if temperature > 25:
+# print(" the weather is nice")
+# elif temperature < 37:
+# print("reduce the temperature")
+
+
+increasement_income = True
+low_income = True
+
+
+if increasement_income and low_income:
+    print("not eligible")
+else:
+    print("eligible")
